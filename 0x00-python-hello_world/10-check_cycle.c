ad767bc5508634210cac640aad7c46ad44a403fd
stoff1180 <stoffmustapha1180@gmail.com>
2023-10-30 10:29:08 -0700

    mandatory

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,13 +9,13 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *slow = list;
+	listint_t *fast = list->next;
+
 	if (list == NULL)
 	{
 		return (0);
 	}
-	listint_t *slow = list;
-	listint_t *fast = list->next;
-
 	while (fast != NULL && fast->next != NULL)
 	{
 		if (slow == fast)
