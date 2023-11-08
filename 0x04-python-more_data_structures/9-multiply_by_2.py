37297ccf04e1315955e873a7dc2541399ee5baed
stoff1180 <stoffmustapha1180@gmail.com>
2023-11-08 15:39:02 -0800

    mandatory

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {key: a_dictionary[key] * 2 for key in a_dictionary}
+    doubled_dict = {}
+    for key, val in a_dictionary.items():
+        doubled_dict[key] = val * 2
+    return doubled_dict
