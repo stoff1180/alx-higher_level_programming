4142f5d3a948dbfaa209664eac4cb07837d6ac2c
stoff1180 <stoffmustapha1180@gmail.com>
2023-11-20 07:30:06 -0800

    mandatory

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        ar = fct(*args)
+        return ar
+    except Exception as err:
+        print("Exception: {}".format(err), file = sys.stderr)
+        return None
