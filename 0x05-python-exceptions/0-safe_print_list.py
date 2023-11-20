0a3b1b3e49739b018a4b7227c2ff36f994c7ebb6
stoff1180 <stoffmustapha1180@gmail.com>
2023-11-20 07:44:20 -0800

    mandatory

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         while j < x:
             print(my_list=[], end='')
-            j+=1
-    except IndexError :
+            j += 1
+    except IndexError:
         None
     print()
     return j
