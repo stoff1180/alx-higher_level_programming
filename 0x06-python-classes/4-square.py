a8ca56b0eb2abf01dbb718c5ef19b193e72c33f0
stoff1180 <stoffmustapha1180@gmail.com>
2023-11-21 07:08:27 -0800

    mandatory

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Property for the current size of the square.
+        Raises:
+            TypeError: if size is not integer.
+            ValueError: if size is less tha 0;
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Area of the Square.
+        Return: the current area of the square.
+        """
+        return (self.__size * self.__size)
