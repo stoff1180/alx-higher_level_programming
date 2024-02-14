20bb3b11f805507e1569860d4edf40771d4e6f03
stoff1180 <stoffmustapha1180@gmail.com>
2024-02-14 14:04:24 -0800

    advanced

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+let dict = require('./101-data').dict;
+
+let totalist = Object.entries(dict);
+let vals = Object.values(dict);
+let valsUniq = [...new Set(vals)];
+let newDict = {};
+for (let j in valsUniq) {
+  let list = [];
+  for (let k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
+    }
+  }
+  newDict[valsUniq[j]] = list;
+}
+console.log(newDict);
