bdaaf43b6619838c048ccf195a3bdcdacade70c5
stoff1180 <stoffmustapha1180@gmail.com>
2024-02-29 02:41:38 -0800

    fixed

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A script that takes in a URL, sends a GET request
-# Displays only the  body of a 200 status code response
+# A script that takes in a URL, sends a GETrequest and displays the  body of a 200 status code response
 curl -Ls "$1"
