796f343a9440c00b904651951f49529a6d85cd6a
stoff1180 <stoffmustapha1180@gmail.com>
2024-03-01 15:36:49 -0800

    fixed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,12 +10,12 @@ import sys
 def main():
     username = sys.argv[1]
     access_token = sys.argv[2]
-    api_url = f'https://api.github.com/user'
+    api_url = 'https://api.github.com/user/{}'.format(username)
     response = requests.get(api_url, auth=(username, access_token))
     if response.status_code == 200:
         user_data = response.json()
         user_id = user_data['id']
-        print(user_id)
+        print('{}'.format(user_id))
 
 
 if __name__ == "__main__":
