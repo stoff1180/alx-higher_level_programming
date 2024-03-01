c234cb13eb145e9ee5d1dab148c8dca61228061d
stoff1180 <stoffmustapha1180@gmail.com>
2024-03-01 14:04:03 -0800

    fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,12 +6,9 @@ import urllib.request
 
 url = 'https://alx-intranet.hbtn.io/status'
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("- Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
-except urllib.error.URLError as e:
-    print("Error fetching URL:", e)
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("- Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
