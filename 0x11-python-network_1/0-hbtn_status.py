5189e8dae918b8bd53ed203c42e0cbd33e5767bf
stoff1180 <stoffmustapha1180@gmail.com>
2024-03-01 07:00:56 -0800

    mandatory

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,6 @@ import urllib.request
 
 if __name__ == '__main__':
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        c = res.read()
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8\
+                content: {}".format(type(c), c, c.decode('utf-8')))
