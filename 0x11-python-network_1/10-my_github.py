1519a0045b954a515102d07076a6609e67165a80
stoff1180 <stoffmustapha1180@gmail.com>
2024-03-01 15:13:43 -0800

    fixed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-
+"""
+Script that takes your GitHub credentials (username and password)
+Uses the GitHub API to display your id
+"""
 import requests
 import sys
 
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <username> <personal_access_token>")
-        sys.exit(1)
 
+def main():
     username = sys.argv[1]
     access_token = sys.argv[2]
     api_url = f'https://api.github.com/user'
+    response = requests.get(api_url, auth=(username, access_token))
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data['id']
+        print(user_id)
 
-    try:
-        response = requests.get(api_url, auth=(username, access_token))
-        if response.status_code == 200:
-            user_data = response.json()
-            user_id = user_data['id']
-            print("Your GitHub user id is:", user_id)
-        else:
-            print("Failed to fetch user data. Status code:", response.status_code)
-    except requests.RequestException as e:
-        print("Error:", e)
 
 if __name__ == "__main__":
     main()
-
