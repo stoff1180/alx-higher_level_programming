396622dde268ca378075e661a25ee239952a5183
stoff1180 <stoffmustapha1180@gmail.com>
2024-03-01 16:16:08 -0800

    fixed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,16 +7,15 @@ import requests
 import sys
 
 
-def main():
+if __name__ == "__main__":
     username = sys.argv[1]
     access_token = sys.argv[2]
     api_url = "https://api.github.com/user".format()
-    response = requests.get(api_url, auth=(username, access_token))
-    user_data = response.json()
-    if response.status_code == 200:
-        user_id = user_data["id"]
-        print("{}".format(user_id))
-
-
-if __name__ == "__main__":
-    main()
+    try:
+        response = requests.get(api_url, auth=(username, access_token))
+        user_data = response.json()
+        if response.status_code == 200:
+            user_id = user_data["id"]
+            print("{}".format(user_id))
+    except IndexError:
+        pass
