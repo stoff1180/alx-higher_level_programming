4cacfc0df17430898e25d78ea540b854bd93b6d3
stoff1180 <stoffmustapha1180@gmail.com>
2024-03-02 14:45:13 -0800

    fixed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,11 +11,13 @@ if __name__ == "__main__":
     username = sys.argv[1]
     access_token = sys.argv[2]
     api_url = "https://api.github.com/user".format()
+    headers = {'Authorization': 'Bearer {}'.format(access_token),
+               'Accept': 'application/vnd.github.v3+json'}
     try:
-        response = requests.get(api_url, auth=(username, access_token))
+        response = requests.get(api_url, headers=headers)
         user_data = response.json()
         if response.status_code == 200:
             user_id = user_data["id"]
             print("{}".format(user_id))
     except IndexError:
-        pass
+        print({}.format(user_data["message"]))
