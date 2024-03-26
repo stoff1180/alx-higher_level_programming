6e263b881184e9911df7609855b8011c22211d6f
stoff1180 <stoffmustapha1180@gmail.com>
2024-03-26 09:26:03 -0700

    Star wars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,10 +3,12 @@ const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
-    console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
+    let count = 0;
+    results.forEach(movie => {
+      if (movie.characters && movie.characters.some(character => character.endsWith('/18/'))) {
+        count++;
+      }
+    });
+    console.log(count);
   }
 });
