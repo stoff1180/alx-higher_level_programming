a9046cefa2bd09238f3c8606c1cd17510bc8bf2e
stoff1180 <stoffmustapha1180@gmail.com>
2024-03-26 08:49:26 -0700

    Read_me

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-    if (error) {
-      console.log(error);
-      return;
-    }
-    console.log(content);
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(content);
 });
